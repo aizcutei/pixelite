bc02d269e6e9aab2644dba8c23ed46ecb4405c8e
Charles Zhang <aiz.cutei@gmail.com>
2022-09-26 20:06:47 -0500

    init environment

--- src/app.rs
+++ src/app.rs
@@ -4,10 +4,16 @@
 pub struct TemplateApp {
     // Example stuff:
     label: String,
+    setting_window: bool,
+    input_window: bool,
+    output_window: bool,
 
     // this how you opt-out of serialization of a member
     #[serde(skip)]
     value: f32,
+
+    #[serde(skip)]
+    texture: Option<egui::TextureHandle>,
 }
 
 impl Default for TemplateApp {
@@ -16,6 +22,10 @@ impl Default for TemplateApp {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            setting_window: true,
+            input_window: true,
+            output_window: true,
+            texture: None,
         }
     }
 }
@@ -45,72 +55,90 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            label,
+            setting_window,
+            input_window,
+            output_window,
+            value,
+            texture,
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
         // Tip: a good default choice is to just keep the `CentralPanel`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
             egui::menu::bar(ui, |ui| {
+                egui::global_dark_light_mode_switch(ui);
                 ui.menu_button("File", |ui| {
+                    if ui.button("Open").clicked() {
+                        todo!()
+                    }
+                    if ui.button("Save as").clicked() {
+                        todo!()
+                    }
+
+                    #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
                     if ui.button("Quit").clicked() {
                         _frame.close();
                     }
                 });
+                ui.menu_button("Window", |ui| {
+                    if ui.button("Setting").clicked() {
+                        *setting_window = !*setting_window;
+                    }
+                    if ui.button("Input").clicked() {
+                        *input_window = !*input_window;
+                    }
+                    if ui.button("Output").clicked() {
+                        *output_window = !*output_window;
+                    }
+                })
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+        if *setting_window {
+            egui::SidePanel::left("side_panel").show(ctx, |ui| {
+                ui.heading("Setting Panel");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
+                    ui.label("Write something: ");
+                    ui.text_edit_singleline(label);
+                });
+
+                ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
+                if ui.button("Increment").clicked() {
+                    *value += 1.0;
+                }
+
+                ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                    ui.horizontal(|ui| {
+                        ui.spacing_mut().item_spacing.x = 0.0;
+                        ui.label("create by ");
+                        ui.hyperlink_to("aizcutei", "https://aizcutei.com");
+                        ui.label(".");
+                    });
                 });
             });
-        });
+        }
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
+            if *input_window {
+                egui::Window::new("Input Image").show(ctx, |ui| {
+                    ui.label("This is a window");
+                    egui::warn_if_debug_build(ui);
+                });
+            }
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
+            if *output_window {
+                egui::Window::new("Output Image").show(ctx, |ui| {
+                    ui.label("This is a window");
+                    egui::warn_if_debug_build(ui);
+                });
+            }
         });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "pixelite",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(pixelite::TemplateApp::new(cc))),
     );
 }
 
@@ -28,7 +28,7 @@ fn main() {
     eframe::start_web(
         "the_canvas_id", // hardcode it
         web_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(pixelite::TemplateApp::new(cc))),
     )
     .expect("failed to start eframe");
 }
